bf05a98478f8c1ad3e8e0c69f7581c3b70417488
Chris Anselmo <canselmo@hybridhealth.com>
2017-05-21 13:17:37 -0400

    Testing proper formatting

--- src/Renderer/renderer.js
+++ src/Renderer/renderer.js
@@ -2,6 +2,23 @@ const { clipboard } = require('electron');
 const beautify_js = require('js-beautify');
 const lang = require('language-classifier');
 
-function readClip() {
-  console.log(beautify_js.js_beautify(clipboard.readText()));
+function formatClipboard() {
+  let clipboard_contents = clipboard.readText(),
+      language = lang(clipboard_contents),
+      output = '';
+
+  switch(language) {
+    case 'css':
+      output = beautify_js.css(clipboard_contents);
+      break;
+    case 'html':
+      output = beautify_js.html(clipboard_contents);
+      break;
+    case 'javascript':
+    default:
+      output = beautify_js.js_beautify(clipboard_contents);
+      break;
+  }
+
+  console.log(output);
 }
